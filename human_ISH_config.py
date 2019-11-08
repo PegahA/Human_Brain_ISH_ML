fbb3d23b2224e1852f45cf7142b542fbe3cfc5a9
Pegah Abed <pegah_abed@Mac.local>
2019-11-08 12:33:35 -0500

    defining variables

---
+++ human_ISH_config.py
@@ -0,0 +1,20 @@
+
+
+
+
+LIST_OF_STUDIES = ["neurotransmitter", "cortex" , "subcortex", "schizophrenia", "autism"]
+STUDY = "cortex"
+MAIN_DIR = "/Users/pegah_abed/Documents/Human_ISH"
+
+
+PATCH_HEIGHT = 256
+PATCH_WIDTH = 256
+NUMBER_OF_CIRCLES_IN_HEIGHT = 2
+NUMBER_OF_CIRCLES_IN_WIDTH = 1
+
+
+TEST_SPLIT = 10
+VALIDATION_SPLIT = 10
+TRAINING_SPLIT = 100 - (TEST_SPLIT + VALIDATION_SPLIT)
+
+
