40e088dd8b0ce63afa748be47c8be26a00d74b6f
Pegah Abed <pegah_abed@Mac.local>
2019-11-08 12:33:50 -0500

    entry point

---
+++ main.py
@@ -0,0 +1,9 @@
+from human_ISH_config import *
+import extract_data
+import process
+import crop_and_rotate
+
+if __name__ == "__main__":
+    #extract_data.run()
+    #crop_and_rotate.run()
+    process.run()
