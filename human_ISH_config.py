92c339130b6a76b10be3875079ff8f8c2d3dd499
Pegah Abed <pegah_abed@Mac.local>
2019-11-11 11:56:04 -0500

    added default values for the parameters

--- human_ISH_config.py
+++ human_ISH_config.py
@@ -1,11 +1,16 @@
-
+import os
 
 
 
 LIST_OF_STUDIES = ["neurotransmitter", "cortex" , "subcortex", "schizophrenia", "autism"]
 STUDY = "cortex"
-MAIN_DIR = "/Users/pegah_abed/Documents/Human_ISH"
+DATA_DIR = "/Users/pegah_abed/Documents/old_Human_ISH"
+CODE_DIR = "/Users/pegah_abed/Documents/Human_Brain_ISH"
+PATCH_TYPE = 'r_per_image'    # other option: 'r_overall'
 
+TEST_SPLIT = 10
+VALIDATION_SPLIT = 10
+TRAINING_SPLIT = 100 - (TEST_SPLIT + VALIDATION_SPLIT)
 
 PATCH_HEIGHT = 256
 PATCH_WIDTH = 256
@@ -13,8 +18,38 @@ NUMBER_OF_CIRCLES_IN_HEIGHT = 2
 NUMBER_OF_CIRCLES_IN_WIDTH = 1
 
 
-TEST_SPLIT = 10
-VALIDATION_SPLIT = 10
-TRAINING_SPLIT = 100 - (TEST_SPLIT + VALIDATION_SPLIT)
+if PATCH_TYPE == 'r_per_image':
+    IMAGE_ROOT = os.path.join(DATA_DIR, STUDY, "per_image_r_patches")
+    EXPERIMENT_ROOT = os.path.join(DATA_DIR, STUDY, "per_image_r_embeddings")
+    if (not os.path.exists(EXPERIMENT_ROOT)):
+        os.mkdir(EXPERIMENT_ROOT)
+
+elif  PATCH_TYPE == 'r_overall' :
+    IMAGE_ROOT = os.path.join(DATA_DIR, STUDY, "overall_r_patches")
+    EXPERIMENT_ROOT = os.path.join(DATA_DIR, STUDY, "overall_r_embeddings")
+    if (not os.path.exists(EXPERIMENT_ROOT)):
+        os.mkdir(EXPERIMENT_ROOT)
 
 
+TRAIN_SET =  os.path.join(DATA_DIR, STUDY, "sets", "triplet_training.csv")
+INITIAL_CHECKPOINT = os.path.join(CODE_DIR, "resnet_v1_50", "resnet_v1_50.ckpt")
+MODEL_NAME = 'resnet_v1_50'
+HEAD_NAME = 'fc1024'
+EMBEDDING_DIM = 128
+BATCH_P = 50
+BATCH_K = 2
+NET_INPUT_HEIGHT = PATCH_HEIGHT  # do you want to try 240?
+NET_INPUT_WIDTH = PATCH_WIDTH # do you want to try 240?
+PRE_CROP_HEIGHT = PATCH_HEIGHT
+PRE_CROP_WIDTH = PATCH_WIDTH
+LOADING_THREADS = 8
+MARGIN = 'soft'
+METRIC = 'euclidean'
+LOSS = 'batch_hard'
+LEARNING_RATE = 3e-4
+TRAIN_ITERATIONS = 25000
+DECAY_START_ITERATION = 15000
+CHECKPOINT_FREQUENCY = 1000
+FLIP_AUGMENT = False
+CROP_AUGMENT = False
+DETAILED_LOGS = False
