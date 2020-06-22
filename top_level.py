2dc2becee6a0182b5c56748f375372a1014a2883
Pegah <pegah.abed73@gmail.com>
2020-06-22 13:34:07 -0500

    latest top

--- top_level.py
+++ top_level.py
@@ -5,24 +5,27 @@ import os
 
 if __name__ == "__main__":
 
-    batch_p_list = [5, 10, 20, 40, 50, 60]
+    batch_p_list = [50,60] #[5, 10, 20, 40, 50, 60]
 
     learning_rate_list = [5e-5, 10e-4, 10e-5, 10e-6]
 
-    #flip_list = [TRUE, FALSE]
+    flip_list = [True, False]
 
-    main_py_path = os.path.join(CODE_DIR, "Human_Brain_ISH_ML", "main.py")
+    main_py_path = os.path.join(CODE_DIR, "main.py")
     for lr in learning_rate_list:
         for batch_p in batch_p_list:
+            for flip in flip_list:
 
-            print ("Here in the top level loop ...")
-            batch_k = 300 // batch_p
+                print ("Here in the top level loop ...")
+                batch_k = 300 // batch_p
 
-            main_command_line_string = "python " + main_py_path + \
-                                        " --train_batch_p=" + str(batch_p) + \
-                                        " --train_batch_k=" + str(batch_k) +\
-                                        " --learning_rate=" +  str(lr)
-            os.system(main_command_line_string)
+                main_command_line_string = "python " + main_py_path + \
+                                            " --train_batch_p=" + str(batch_p) + \
+                                            " --train_batch_k=" + str(batch_k) +\
+                                            " --learning_rate=" +  str(lr) +\
+                                            " --train_flip_augment=" + str(flip)
+
+                os.system(main_command_line_string)
 
 
 
