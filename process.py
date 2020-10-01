12363f0292ab9e50e78d66761157108322e65adc
Pegah Abed <pegah_abed@Mac.local>
2020-10-01 18:37:43 -0400

    get new columns for top models validation embeddings

--- process.py
+++ process.py
@@ -1843,7 +1843,7 @@ def add_new_columns_to_image_level_embed_file(ts, columns, study=None):
     image_level_file_name = ""
     for item in contents:
         if study == None:
-            if item.endswith("training_validation_embeddings_image_level.csv"):
+            if item.endswith("validation_embeddings_image_level.csv"):
                 image_level_file_name = item
                 images_info = pd.read_csv(os.path.join(DATA_DIR, STUDY, "human_ISH_info.csv"))
 
@@ -2146,4 +2146,6 @@ if __name__ == '__main__':
     #path_to_csv = "/Users/pegah_abed/Documents/old_Human_ISH/after_segmentation/dummy_2/SZ/1596374295_minus_43_triplet_patches_schizophrenia_embeddings_image_level_with_info.csv"
     #convert_to_tsv_meta_and_without_meta(path_to_csv)
 
-    generate_random_embeddings(embeddings_length=128)
+    #generate_random_embeddings(embeddings_length=128)
+
+    add_new_columns_to_image_level_embed_file("1596374295", ["donor_id", "gene_symbol", "region"])
